35f0c544462eccdb5722a0afab2a2bdc92d54ea0
Tim <tbatty7@gmail.com>
2020-11-05 23:33:08 -0500

    fixed packer pushing using puppet master-agent setup

--- puppet_master_slave/install-puppet.sh
+++ puppet_master_slave/install-puppet.sh
@@ -2,24 +2,28 @@
 
 echo "****************** Setting Hostname"
 sleep 10
-sudo hostnamectl set-hostname puppet-agent-2.test.org
+sudo hostname
 
 echo "****************** Adding puppetmaster to /etc/hosts file"
-echo 172.31.32.221 puppet-master.test.org | sudo tee -a /etc/hosts
-sudo ping -c 5 puppet-master.test.org
+echo 172.31.32.221 puppet | sudo tee -a /etc/hosts
+sudo ping -c 5 puppet
 sudo cat /etc/hosts
 
 echo "****************** Updating apt package info..."
-sudo apt-get update 
-sudo apt-get upgrade -y
+sudo apt update 
+sudo apt upgrade -y
 sleep 15
 
 echo "****************** Installing Puppet"
-sudo apt-get install puppet -y
+sudo apt install puppet -y
 
 echo "****************** Setting puppet master server"
-sudo puppet config set server puppet-master.test.org --section main 
+sudo puppet config set server puppet --section main 
+
+echo "****************** Setting certificate name"
+sudo puppet config set certname $(hostname) --section main
 
 echo "****************** Checking config"
 sudo cat /etc/puppet/puppet.conf
+sudo ls /var/lib/puppet
 exit
